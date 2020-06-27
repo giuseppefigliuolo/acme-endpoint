c0df060eb35416a933505b8718517e823cb77984
giuseppefigliuolo <figliuologiuseppe@gmail.com>
2020-06-27 15:58:53 +0300

    aaggiornamento

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 const data = require("./data.json");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.get("", (req, res) => {
   res.json(data);
 });
